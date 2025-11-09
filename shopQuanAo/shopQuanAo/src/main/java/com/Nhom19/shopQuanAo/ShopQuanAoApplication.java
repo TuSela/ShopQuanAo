55fe4256a78a66de570a3dec0fc667aefd55aa8f
TuSela <tusela022@gmail.com>
2025-11-10 00:52:01 +0700

    Cau hinh Foder

---
+++ shopQuanAo/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/ShopQuanAoApplication.java
@@ -0,0 +1,13 @@
+package com.Nhom19.shopQuanAo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopQuanAoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopQuanAoApplication.class, args);
+	}
+
+}
