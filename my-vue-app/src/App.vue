32e037a2c124e558fd764804bd3c1114a8a0ef6d
TuSela <tusela022@gmail.com>
2025-12-17 10:47:31 +0700

    Admin them san pham

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -4,10 +4,5 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div
-    class="min-h-screen bg-blue-600 text-white flex items-center justify-center text-3xl font-bold"
-  >
-    TAILWIND ĐÃ HOẠT ĐỘNG
-  </div>
   <UserList />
 </template>
