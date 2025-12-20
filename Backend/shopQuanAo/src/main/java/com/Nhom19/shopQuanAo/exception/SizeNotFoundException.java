f8a95fd83ed7d7740c1c3ad448c9d7a28bdb6035
PeterVII23 <shirouemiya720@gmail.com>
2025-12-20 15:02:42 +0700

    ko can thiet

--- Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/exception/SizeNotFoundException.java
+++ Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/exception/SizeNotFoundException.java
@@ -1,7 +1,4 @@
 package com.Nhom19.shopQuanAo.exception;
 
 public class SizeNotFoundException extends RuntimeException {
-    public SizeNotFoundException() {
-        super("Size not found");
-    }
 }
