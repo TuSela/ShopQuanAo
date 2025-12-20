ac946d1e756f51e2e9e8c3250f0c325b15753bef
PeterVII23 <shirouemiya720@gmail.com>
2025-12-20 15:01:30 +0700

    them annotation

--- Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/repository/ProductTypeRepo.java
+++ Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/repository/ProductTypeRepo.java
@@ -2,7 +2,9 @@ package com.Nhom19.shopQuanAo.repository;
 
 import com.Nhom19.shopQuanAo.entity.ProductTypes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductTypeRepo extends JpaRepository<ProductTypes, Integer> {
     boolean existsByTenLoai(String tenLoai);
 }
