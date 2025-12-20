355a303952a5d787e20d75c6717c0a8db36eabc3
PeterVII23 <shirouemiya720@gmail.com>
2025-12-20 14:59:08 +0700

    ko can thiet lam

--- Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/exception/DuplicateSizeException.java
+++ Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/exception/DuplicateSizeException.java
@@ -1,7 +1,4 @@
 package com.Nhom19.shopQuanAo.exception;
 
 public class DuplicateSizeException extends RuntimeException {
-    public DuplicateSizeException() {
-        super("Size already exists");
-    }
 }
