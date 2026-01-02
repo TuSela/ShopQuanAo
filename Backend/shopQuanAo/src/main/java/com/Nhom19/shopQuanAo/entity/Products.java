a8ba7f60fa520e5736e079179d83855237afbc7b
PeterVII23 <shirouemiya720@gmail.com>
2026-01-02 15:51:13 +0700

    them cot trangthai cho products

--- Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/entity/Products.java
+++ Backend/shopQuanAo/src/main/java/com/Nhom19/shopQuanAo/entity/Products.java
@@ -26,6 +26,7 @@ public class Products {
     private BigDecimal gia;
     private String chiTiet;
     private Float danhGia;
+    private Boolean trangThai = true;
 
     @OneToMany(mappedBy = "products", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductVariants> productVariants;
